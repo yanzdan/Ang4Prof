c6043b164e8716927d3df1d38a93e48a1c421f55
yanzdan <42217485+yanzdan@users.noreply.github.com>
2018-08-22 21:05:01 +0300

    q

--- Ang4Prof.05/primer.js
+++ Ang4Prof.05/primer.js
@@ -1,9 +1,21 @@
 //MyFunc();
-var MyFunc = function () {
-    console.log("Hello from function!");
+var MyFunc = function (name, weather) {
+    console.log("Hello, " + name + " !");
+    console.log("It is " + weather + " today");
 };
-MyFunc();
-SecondFunc();
-function SecondFunc() {
-    console.log('Buy from second');
+var Func4Array = function (value, index, xarr) { console.log("IFunc " + index + ": " + value + " of (" + this + ")"); };
+function SecondFunc(value, index, xarr) { console.log("SecondFunc " + index + ": " + value + " of (" + this + ")"); }
+;
+MyFunc("Tom", "rain");
+var StringVal = "1.2.3";
+var NumVal = Number(StringVal);
+console.log("Number(\"" + StringVal + "\") = " + Number(StringVal));
+console.log("parseFloat(\"" + StringVal + "\") = " + parseFloat(StringVal));
+console.log("parseInt(\"" + StringVal + "\") = " + parseInt(StringVal));
+var myArray = [100, "Adam", true];
+for (var i = 0; i < myArray.length; i++) {
+    console.log("myArray(" + i + ")=<<" + myArray[i] + ">>");
 }
+console.log("---");
+//myArray.forEach((value, index, xarr) => console.log("Index " + index + ": " + value + " of (" +this+")"));
+myArray.forEach(SecondFunc);
--- Ang4Prof.05/primer.ts
+++ Ang4Prof.05/primer.ts
@@ -2,13 +2,33 @@
 
 
 
-let MyFunc = function(){
-  console.log("Hello from function!");
+let MyFunc = function( name, weather){
+  console.log(`Hello, ${name} !`);
+  console.log("It is " + weather + " today");
+
 };
 
-MyFunc();
-SecondFunc();
+let Func4Array = function (value, index, xarr) { console.log("IFunc " + index + ": " + value + " of (" +this+")")};
 
-function SecondFunc() {
-  console.log('Buy from second');
+
+function SecondFunc (value, index, xarr) { console.log("SecondFunc " + index + ": " + value + " of (" +this+")")};
+
+MyFunc("Tom", "rain");
+
+
+let StringVal = "1.2.3";
+let NumVal = Number(StringVal);
+console.log(`Number("${StringVal}") = ${Number(StringVal)}`);
+console.log(`parseFloat("${StringVal}") = ${parseFloat(StringVal)}`);
+console.log(`parseInt("${StringVal}") = ${parseInt(StringVal)}`);
+
+
+let myArray = [100, "Adam", true];
+for (let i = 0; i < myArray.length; i++) {
+  console.log("myArray(" + i + ")=<<" + myArray[i] + ">>");
 }
+console.log("---");
+//myArray.forEach((value, index, xarr) => console.log("Index " + index + ": " + value + " of (" +this+")"));
+myArray.forEach(SecondFunc);
+
+
