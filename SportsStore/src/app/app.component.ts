1f91f3bdb1798058f49cb398b9efe59e9734c403
yanzdan <42217485+yanzdan@users.noreply.github.com>
2018-09-06 07:56:52 +0300

    working version from book

--- SportsStore/src/app/app.component.ts
+++ SportsStore/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<store></store>`
+  // template: `<store></store>`
+  template: `<div class="bg-success p-a-1 text-xs-center">
+              This is SportsStore
+             </div>`
 })
 export class AppComponent { }
