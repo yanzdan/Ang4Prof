23419e4e9f7ba7040866ebb297c80873e9fb7a7c
yanzdan <42217485+yanzdan@users.noreply.github.com>
2018-09-13 17:24:14 +0300

    a

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -8,14 +8,25 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
   public  selectedCategory = null;
-  public  selectedPage = 1;
-  public  productsPerPage = 4;
+  private  _selectedPage = 1;
+  private  _productsPerPage = 4;
 
   constructor(private repository: ProductRepository) { }
+  get selectedPage():number {
+    return this._selectedPage;
+  }
+
+  set selectedPage(value:number) {
+    this._selectedPage = value;
+  }
+
+  get productsPerPage(): number {
+    return this._productsPerPage;
+  }
   get products(): Product[] {
-    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    let pageIndex = (this._selectedPage - 1) * this._productsPerPage
     return this.repository.getProducts(this.selectedCategory)
-      .slice(pageIndex, pageIndex + this.productsPerPage);
+      .slice(pageIndex, pageIndex + this._productsPerPage);
   }
   get categories(): string[] {
     return this.repository.getCategories();
@@ -26,15 +37,19 @@ export class StoreComponent {
   }
 
   changePage(newPage: number) {
-    this.selectedPage = newPage;
+    this._selectedPage = newPage;
   }
   changePageSize(newSize: number) {
-    this.productsPerPage = Number(newSize);
+    this._productsPerPage = Number(newSize);
     this.changePage(1);
   }
   get pageNumbers(): number[] {
     return Array(Math.ceil(this.repository
-      .getProducts(this.selectedCategory).length / this.productsPerPage))
+      .getProducts(this.selectedCategory).length / this._productsPerPage))
       .fill(0).map((x, i) => i + 1);
   }
+  get pageCount():number  {
+    return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this._productsPerPage);
+  }
+
 }
